89e5f41780e3857f3fbbac459411148ea836a776
Migue <126858386+Migueortiiz@users.noreply.github.com>
2024-05-08 18:09:02 +0200

    first steps home

---
+++ src/javafxmlapplication/HomeController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author migue
+ */
+public class HomeController implements Initializable {
+
+    @FXML
+    private Button buttonClick;
+    @FXML
+    private Label labelMessage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+}
