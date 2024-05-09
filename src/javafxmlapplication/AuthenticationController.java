165c316affe813c72233c1400fd2ca325231a20f
felixdl8 <felixbusiness80@gmail.com>
2024-05-09 17:25:59 +0200

    Login & Signup basic

--- src/javafxmlapplication/AuthenticationController.java
+++ src/javafxmlapplication/AuthenticationController.java
@@ -4,17 +4,39 @@
  */
 package javafxmlapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author felix
- */
 public class AuthenticationController implements Initializable {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    public void switchToLogin(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void switchToSignup(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("Signup.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     /**
      * Initializes the controller class.
      */
--- src/javafxmlapplication/JavaFXMLApplication.java
+++ src/javafxmlapplication/JavaFXMLApplication.java
@@ -18,7 +18,7 @@ public class JavaFXMLApplication extends Application {
     public void start(Stage stage) throws Exception {
         //======================================================================
         // 1- creación del grafo de escena a partir del fichero FXML
-        FXMLLoader loader= new  FXMLLoader(getClass().getResource("Authentication.fxml"));
+        FXMLLoader loader= new  FXMLLoader(getClass().getResource("Login.fxml"));
         Parent root = loader.load();
         //======================================================================
         // 2- creación de la escena con el nodo raiz del grafo de escena
