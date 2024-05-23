158bfc0ca7cc885b7aa5d299ab6efee3701782e0
Migue <126858386+Migueortiiz@users.noreply.github.com>
2024-05-23 16:56:08 +0200

    PERMA PUSH, felix eres un capullo

---
+++ src/javafxmlapplication/LoginController.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import model.Acount;
+import model.AcountDAOException;
+
+/**
+ * FXML Controller class
+ *
+ * @author migue
+ */
+public class LoginController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    private String username = "";
+    private String password = "";
+    
+    private Acount account;
+
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private MenuButton languageMenuB;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            account = Acount.getInstance();
+        } catch (AcountDAOException | IOException ex) {
+            System.out.println(ex);
+        }
+        
+        initializeMenuButton();
+        
+    }    
+    
+    
+    //All methods
+    
+    @FXML
+    private void usernameInput(KeyEvent event) {
+        username = usernameField.getText();
+    }
+
+    @FXML
+    private void passwordInput(KeyEvent event) {
+        password = passwordField.getText();
+    }
+
+    @FXML
+    private void loginUser(ActionEvent event) throws IOException {
+        try {
+            if (!account.logInUserByCredentials(username, password)) {
+                errorLabel.textProperty().set("User or password are wrong");
+                errorLabel.visibleProperty().set(true);
+                return;
+            }
+        } catch (AcountDAOException ex) {
+            System.out.println(ex);
+        }
+        
+        root = FXMLLoader.load(getClass().getResource("Home.fxml"), ShifuApp.getResourceBundle());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void switchToSignup(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Signup.fxml"), ShifuApp.getResourceBundle());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    private void initializeMenuButton() {
+        Locale[] availableLanguages = ShifuApp.availableLanguages;
+        ImageView imageMain = new ImageView("/resources/images/" + ShifuApp.getLocaleString() + ".png");
+        imageMain.setFitWidth(30);
+        imageMain.setFitHeight(20);
+        languageMenuB.setGraphic(imageMain);
+        for (int i = 0; i < availableLanguages.length; i++) {
+            Locale auxLocale = availableLanguages[i];
+            ImageView image = new ImageView("/resources/images/" + auxLocale.toString() + ".png");
+            image.setFitWidth(30);
+            image.setFitHeight(20);
+            MenuItem auxMenuItem = new MenuItem("",image);
+            auxMenuItem.onActionProperty().set(e -> {
+                ShifuApp.setLocale(auxLocale);
+                try {
+                    root = FXMLLoader.load(getClass().getResource("Login.fxml"), ShifuApp.getResourceBundle());
+                } catch (IOException ex) {
+                    Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                stage = (Stage) languageMenuB.getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            });
+            languageMenuB.getItems().add(auxMenuItem);
+        }
+    }
+    
+}
