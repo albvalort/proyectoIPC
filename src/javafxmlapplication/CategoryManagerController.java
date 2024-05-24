e362e16169d7d1e3edb9ade6b8ecf845f0986721
Migue <126858386+Migueortiiz@users.noreply.github.com>
2024-05-24 14:16:37 +0200

    Resolvido la conexione del pop up categorico

---
+++ src/javafxmlapplication/CategoryManagerController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author migue
+ */
+public class CategoryManagerController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private Button backButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+  
+
+    @FXML
+    private void closeCategoryManager(ActionEvent event) {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+       stage.close();
+    }
+
+    
+
+    
+}
--- src/javafxmlapplication/ChargeManagerController.java
+++ src/javafxmlapplication/ChargeManagerController.java
@@ -7,6 +7,8 @@ package javafxmlapplication;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,8 +18,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 /**
  * FXML Controller class
  *
@@ -31,6 +35,8 @@ public class ChargeManagerController implements Initializable {
 
     @FXML
     private Button backButton;
+    @FXML
+    private Button categoryButton;
     /**
      * Initializes the controller class.
      */
@@ -39,9 +45,35 @@ public class ChargeManagerController implements Initializable {
         // TODO
     }    
 
+ 
+
+    @FXML
+    private void openCategoryManager(ActionEvent event) {
+        Parent proot = null;
+        try {
+            proot = FXMLLoader.load(getClass().getResource("CategoryManager.fxml"), FXRouter.getResourceBundle());
+            Stage pstage = new Stage();
+            proot.setOnKeyPressed( key -> {
+                if (key.getCode() == KeyCode.ESCAPE) {
+                    pstage.close();
+                }
+            });
+
+            Scene pscene = new Scene(proot);
+            
+            pstage.setScene(pscene);
+            pstage.initStyle(StageStyle.UNDECORATED);
+            pstage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(ProfileSettingsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     @FXML
     private void switchToHome(ActionEvent event) throws IOException {
         FXRouter.goTo("home");
     }
+
+   
 
 }
